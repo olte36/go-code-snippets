89e3cc9ee2b6a5ca715ee64477467fe44cd8c41b
olte36 <hybriddodod+github@gmail.com>
2025-10-27 18:21:07 +0500

    bfs

---
+++ algorithms/bfs.go
@@ -0,0 +1,20 @@
+package algorithms
+
+type TreeNode struct {
+	Val    string
+	Leaves []*TreeNode
+}
+
+func BreadthFirstTravese(root *TreeNode, fn func(*TreeNode) bool) {
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		if curr != nil {
+			if fn(curr) {
+				return
+			}
+			queue = append(queue, curr.Leaves...)
+		}
+	}
+}
